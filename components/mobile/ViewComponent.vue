8f2a43977fb12305ce7b766807b48008bad4dc95
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-03 13:37:24 +0700

    Change svg to png

--- components/mobile/ViewComponent.vue
+++ components/mobile/ViewComponent.vue
@@ -235,16 +235,16 @@ export default {
       //Project
       projects: [
         { id: 1, title: "Portfolio", src:"icon/hero-mobile.webp", to: "https://github.com/devdewa?tab=repositories" },
-        { id: 2, title: "Old Portfolio", src:"icon/old-portfolio.svg", to: "https://dewaprogramming.vercel.app/" },
-        { id: 3, title: "DewaProgramming", src:"icon/dewaprogramming.svg", to: "https://github.com/devdewa/DewaProgramming" },
+        { id: 2, title: "Old Portfolio", src:"icon/old-portfolio.png", to: "https://dewaprogramming.vercel.app/" },
+        { id: 3, title: "DewaProgramming", src:"icon/dewaprogramming.png", to: "https://github.com/devdewa/DewaProgramming" },
         { id: 4, title: "Decorative Plant", src:"icon/decorative-plant.svg", to: "https://decorative-plant.vercel.app/" },
         { id: 5, title: "Learning Centre Indonesia", src:"icon/lci.svg", to: "https://learningcentreindonesia.com/web/" },
-        { id: 6, title: "Wangyland", src:"icon/wangyland.svg", to: "https://wangyland.vercel.app/" },
+        { id: 6, title: "Wangyland", src:"icon/wangyland.png", to: "https://wangyland.vercel.app/" },
         { id: 7, title: "Bang CV", src:"icon/bangcv.webp", to: "https://github.com/devdewa/bangcv" },
         { id: 8, title: "MP3 / MP4 Downloader", src:"icon/github.svg", to: "https://dewa-downloader.vercel.app/" },
         { id: 9, title: "E-Commerce LSP", src:"icon/lsp.webp", to: "https://github.com/devdewa/E-Commerce-LSP" },
         { id: 10, title: "Belajar HTML App", src:"icon/expo.svg", to: "https://github.com/devdewa/React-Native-Expo-Belajar-HTML-App" },
-        { id: 11, title: "Averroes Unipessoal. Lda", src:"icon/averroes.svg", to: "https://www.averroes.info/" },
+        { id: 11, title: "Averroes Unipessoal. Lda", src:"icon/averroes.png", to: "https://www.averroes.info/" },
         { id: 12, title: "Protem", src:"icon/github.svg", to: "https://pt-teknologi-pustaka-indonesia-buku.vercel.app/" },
       ],
 
