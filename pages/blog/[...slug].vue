7d5dbc7a76223b8eb8b81db8fffd5ba05546bdcb
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-05 15:58:01 +0700

    Responsive blog and add button back in blog post

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,11 +1,13 @@
 <template>
   <main class="bg-gray-950 text-gray-400" id="blog">
     <section
-      class="bg-[url('/assets/pattern-hero-left.png')] dark:bg-[url('/assets/pattern-hero-left-dark.png')] bg-contain bg-center px-32"
+      class="bg-[url('/assets/pattern-hero-left.png')] dark:bg-[url('/assets/pattern-hero-left-dark.png')] bg-contain bg-center px-8 lg:px-32"
     >
       <div class="container mx-auto bg-gray-900/20 relative">
-        <button @click.prevent="$router.back()" class="absolute top-7 left-9"><Icon name="icon-park-twotone:back" class="w-5 h-5 lg:w-7 lg:h-7"/></button>
-        <div class="text-muted mx-auto py-12 px-16 w-full max-w-[40rem] bg-gray-900/40">
+        <button @click.prevent="$router.back()" class="absolute top-4 -left-3 bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent lg:top-7 lg:left-9">
+          <Icon name="icon-park-twotone:back" class="text-white w-4 h-4 lg:w-7 lg:h-7" />
+        </button>
+        <div class="text-muted mx-auto py-12 px-8 lg:px-16 w-full lg:max-w-[40rem] bg-gray-900/40">
           <!-- <ContentRenderer :value="data" /> -->
           <ContentDoc />
           <div class="mt-12 space-x-4">
@@ -13,7 +15,9 @@
               v-for="tag in data.tags"
               class="text-xs font-semibold py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase"
             >
-              <Icon name="pajamas:label" size="0.8rem" class="text-gray-100 mr-2 -mt-[0.2rem]" />{{ tag }}
+              <Icon name="pajamas:label" size="0.8rem" class="text-gray-100 mr-2 -mt-[0.2rem]" />{{
+                tag
+              }}
             </span>
           </div>
         </div>
@@ -23,35 +27,34 @@
 </template>
 
 <style>
-  #blog h1 {
-    @apply text-3xl text-center font-bold mb-6;
-  }
-  #blog h2 {
-    @apply text-2xl font-semibold pt-6 pb-2;
-  }
-  #blog h3 {
-    @apply text-sm font-semibold pt-6 pb-2;
-  }
-  #blog p {
-    @apply text-sm leading-loose;
-  }
-  #blog a, #blog strong {
-    @apply text-sm my-4;
-  }
-  #blog li {
-    @apply list-disc ml-5 mt-3 leading-relaxed;
-  }
-  #blog blockquote {
-    @apply bg-gray-800 rounded-md px-4 py-2 my-2 text-xs;
-  }
+#blog h1 {
+  @apply text-2xl lg:text-3xl text-center font-bold mb-6;
+}
+#blog h2 {
+  @apply text-2xl font-semibold pt-6 pb-2;
+}
+#blog h3 {
+  @apply text-sm font-semibold pt-6 pb-2;
+}
+#blog p {
+  @apply text-xs lg:text-sm !leading-loose;
+}
+#blog a,
+#blog strong {
+  @apply text-xs lg:text-sm my-4;
+}
+#blog li {
+  @apply text-xs lg:text-sm list-disc ml-5 mt-3 !leading-loose;
+}
+#blog blockquote {
+  @apply bg-gray-800 rounded-md px-4 py-2 my-2 text-xs;
+}
 </style>
 
 <script setup>
-  const { path } = useRoute();
+const { path } = useRoute();
 
-  const { data } = await useAsyncData('content-${path}', () => {
-    return queryContent()
-      .where({ _path: path })
-      .findOne();
-  });
+const { data } = await useAsyncData("content-${path}", () => {
+  return queryContent().where({ _path: path }).findOne();
+});
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -13,11 +13,14 @@ useSeoMeta({
 <template>
   <main class="bg-gray-950 text-white min-h-screen">
     <section
-      class="bg-[url('/assets/pattern-hero-left.png')] dark:bg-[url('/assets/pattern-hero-left-dark.png')] bg-contain bg-center px-32"
+      class="bg-[url('/assets/pattern-hero-left.png')] dark:bg-[url('/assets/pattern-hero-left-dark.png')] bg-contain bg-center px-8 lg:px-32"
     >
-      <div class="min-h-screen container mx-auto bg-gray-900/50 py-10 px-28">
-        <h1 class="text-md xl:text-3xl 2xl:text-4xl text-center font-bold py-12">Blog Posts</h1>
-        <section class="grid md:grid-cols-3 gap-10 2xl:gap-20 mt-8">
+      <div class="min-h-screen container mx-auto bg-gray-900/50 py-4 lg:py-10 px-8 lg:px-28 relative">
+        <button @click.prevent="$router.back()" class="absolute top-4 -left-3 bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent lg:top-7 lg:left-9">
+          <Icon name="icon-park-twotone:back" class="text-white w-4 h-4 lg:w-7 lg:h-7" />
+        </button>
+        <h1 class="text-2xl xl:text-3xl 2xl:text-4xl text-center font-bold py-6 lg:py-12">Blog Posts</h1>
+        <section class="grid md:grid-cols-3 gap-10 2xl:gap-20 mt-4 lg:mt-8">
           <UIBlogCard :posts="posts" />
         </section>
       </div>
