c9f990ec6077b47ecc43ae052aa3e79235af6d56
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-05 07:58:00 +0700

    Testing-2

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
   const { data: posts } = await useAsyncData("posts", () => 
     queryContent("/blog").find()
   );
@@ -15,6 +15,62 @@
           <UIBlogCard :posts="posts" />
         </section>
       </div>
+    </section>
+  </main>
+</template> -->
+
+<script setup>
+// set meta for page
+useHead({
+  title: "All articles",
+  meta: [{ name: "description", content: "Here's a list of all my great articles" }],
+});
+</script>
+
+<template>
+  <main>
+    <header class="page-heading">
+      <div class="wrapper">
+        <h1 class="text-5xl font-extrabold">All articles</h1>
+        <p class="font-medium text-lg">Here's a list of all my great articles</p>
+      </div>
+    </header>
+    <section class="page-section">
+      <!-- Render list of all articles in ./content/blog using 'path` -->
+      <!-- Provide only defined fields in the `:query` prop -->
+      <ContentList
+        path="/blog"
+        :query="{
+          only: ['title', 'description', 'tags', '_path', 'img'],
+        }"
+      >
+        <!-- Default list slot -->
+        <template v-slot="{ list }">
+          <ul class="article-list">
+            <li v-for="article in list" :key="article._path" class="article">
+              <NuxtLink :to="article._path">
+                <div class="wrapper">
+                  <div class="img-cont w-32">
+                   
+                  </div>
+                  <header>
+                    <h1 class="text-2xl font-semibold">{{ article.title }}</h1>
+                    <p>{{ article.description }}</p>
+                    <ul class="article-tags">
+                      <li class="tag !py-0.5" v-for="(tag, n) in article.tags" :key="n">{{ tag }}</li>
+                    </ul>
+                  </header>
+                </div>
+              </NuxtLink>
+            </li>
+          </ul>
+        </template>
+
+        <!-- slot to display message when no content is found -->
+        <template #not-found>
+          <p>No articles found.</p>
+        </template>
+      </ContentList>
     </section>
   </main>
 </template>
