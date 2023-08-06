b4b49a41fcfc0fc13f8bb9644294496ba3d31d45
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-06 07:16:15 +0700

    Change h-12 to full

--- components/mobile/ViewComponent.vue
+++ components/mobile/ViewComponent.vue
@@ -138,7 +138,7 @@
                         <!-- Project -->
                         <NuxtLink :to="project.to" target="_blank" :title="project.title" class="text-gray-500 dark:text-white  col-span-10 md:col-span-3 cursor-pointer ease-in-out duration-300 hover:scale-105 hover:backdrop-saturate-200 flex justify-center border-dashed border-2 border-gray-400 dark:border-gray-800 pt-4 pb-6 hover:border-gray-100" v-for="project in projects" :key="project.id">
                         <div class="flex justify-center flex-col gap-6">
-                            <img :src="project.src" alt="" class="w-12 h-12 m-auto rounded-full bg-zinc-800 p-1.5 shadow-md border border-zinc-700/50 ring-0" />
+                            <img :src="project.src" alt="" class="w-12 h-full m-auto rounded-full bg-zinc-800 p-1.5 shadow-md border border-zinc-700/50 ring-0" />
                             <div class="flex justify-center items-center">
                                 <Icon name="ph:link-bold" class="w-3 h-3"/><span class="ml-2 text-xs font-semibold">{{ project.title }}</span>
                             </div>
