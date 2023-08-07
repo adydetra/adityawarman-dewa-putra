b2f6e44fa71abcbe44cc8d3280a38f1f9beaaee9
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-07 17:07:38 +0700

    Lazy Component

---
+++ components/mobile/Photo.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="flex justify-center gap-6 overflow-hidden pt-16 pb-12">
+    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl -rotate-3 shadow-2xl shadow-yellow-200/10">
+      <img src="~/assets/hero-1.webp" alt="o" class="absolute h-full w-full object-cover" />
+    </div>
+    <div class="relative aspect-[10/10] w-32 md:w-60 flex-none overflow-hidden rounded-xl">
+      <img src="~/assets/hero-3.webp" alt="o" class="absolute h-full w-full object-scale-down" />
+    </div>
+    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl rotate-3 shadow-2xl shadow-yellow-200/10">
+      <img src="~/assets/hero-2.webp" alt="o" class="absolute h-full w-full object-cover" />
+    </div>
+  </section>
+</template>
--- components/mobile/ViewComponent.vue
+++ components/mobile/ViewComponent.vue
@@ -25,17 +25,7 @@
       </div>
     </section>
 
-    <section class="flex justify-center gap-6 overflow-hidden pt-16 pb-12">
-      <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl -rotate-3 shadow-2xl shadow-yellow-200/10">
-        <img src="~/assets/hero-1.webp" alt="Photo" class="absolute h-full w-full object-cover" />
-      </div>
-      <div class="relative aspect-[10/10] w-32 md:w-60 flex-none overflow-hidden rounded-xl">
-        <img src="~/assets/hero-3.webp" alt="Photo" class="absolute h-full w-full object-scale-down" />
-      </div>
-      <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl rotate-3 shadow-2xl shadow-yellow-200/10">
-        <img src="~/assets/hero-2.webp" alt="Photo" class="absolute h-full w-full object-cover" />
-      </div>
-    </section>
+    <LazyMobilePhoto/>
 
     <!-- Uses -->
     <div class="mx-auto relative px-6 py-8 md:py-20 lg:py-8 bg-[url('/assets/pattern-hero-left.png')] bg-contain bg-center">
