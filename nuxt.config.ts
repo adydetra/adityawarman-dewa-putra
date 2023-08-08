8eebf0dd7aeb7fc8e8da16dd5f4986b7f5ff5f3a
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-08 18:12:26 +0700

    Add prefetch & preconnect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,9 @@ export default defineNuxtConfig({
     families: {
       Poppins: [100, 200, 300, 400, 500, 600, 700],
     },
+    display: "swap",
+    prefetch: true,
+    preconnect: true,
   },
 
   colorMode: {
