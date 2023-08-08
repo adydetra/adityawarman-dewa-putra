3bf0dbbf8492d9e04de59517ad90cd67062e5120
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-08 13:48:07 +0700

    Fix

--- components/UI/Feed/Card.vue
+++ components/UI/Feed/Card.vue
@@ -6,7 +6,7 @@ const props = defineProps(["posts"]);
   <div v-for="post in props.posts" :key="post.slug" class="flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300">
     <div>
       <div class="flex justify-center items-center w-8 md:w-12 rotate-3 rounded-full">
-        <img src="~/assets/hero-mobile.webp" alt="Photo" class="w-full h-full rounded-full shadow-md shadow-gray-500/40" />
+        <img src="images/hero-mobile.webp" alt="Photo" class="w-full h-full rounded-full shadow-md shadow-gray-500/40" />
       </div>
     </div>
     <div class="w-full overflow-hidden space-y-4">
