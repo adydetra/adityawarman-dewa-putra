4b728a43dc98be37aa99a9596c62d89f29f9639b
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-08 18:31:11 +0700

    Update Style

--- components/mobile/Content.vue
+++ components/mobile/Content.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="flex justify-center gap-6 overflow-hidden pt-16 pb-12">
-    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl -rotate-3 shadow-2xl shadow-yellow-200/10">
+    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl shadow-2xl shadow-yellow-200/10 -rotate-3">
       <img src="/images/hero-1.webp" alt="o" class="absolute h-full w-full object-cover" />
     </div>
     <div class="relative aspect-[10/10] w-32 md:w-60 flex-none overflow-hidden rounded-xl">
       <img src="/images/hero-3.webp" alt="o" class="absolute h-full w-full object-scale-down" />
     </div>
-    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl rotate-3 shadow-2xl shadow-yellow-200/10">
+    <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl shadow-2xl shadow-yellow-200/10 rotate-3">
       <img src="/images/hero-2.webp" alt="o" class="absolute h-full w-full object-cover" />
     </div>
   </section>
