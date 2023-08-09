15473add335c82579f97162c50d300f5ef68000d
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-09 15:00:44 +0700

    Test button

--- components/UI/Button/Back.vue
+++ components/UI/Button/Back.vue
@@ -1,9 +1,31 @@
 <template>
-  <button
+  <!-- <button
     @click.prevent="$router.back()"
+    class="fixed top-4 left-4 rounded-md bg-gray-200 ring-1 ring-inset ring-gray-400 dark:ring-0 lg:ring-0 dark:bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent dark:lg:bg-transparent lg:top-7 lg:left-9"
+  >
+    <Icon name="line-md:arrow-left-circle" class="text-black dark:text-white w-4 h-4 lg:w-8 lg:h-8 -mt-1" />
+  </button> -->
+  <button
+  @click="goToPrev()"
     class="fixed top-4 left-4 rounded-md bg-gray-200 ring-1 ring-inset ring-gray-400 dark:ring-0 lg:ring-0 dark:bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent dark:lg:bg-transparent lg:top-7 lg:left-9"
   >
     <Icon name="line-md:arrow-left-circle" class="text-black dark:text-white w-4 h-4 lg:w-8 lg:h-8 -mt-1" />
   </button>
   
 </template>
+
+<script>
+export default {
+  methods: {
+    goToPrev() {
+
+    // ...
+    // Do other logic like logging, etc.
+    // ...
+
+    // Tell router to go back one
+    window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    },
+  },
+}
+</script>
