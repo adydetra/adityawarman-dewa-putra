ac7ccaaa762e172f02bbe35fa95267e7dca63f35
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-10 21:34:52 +0700

    components: add mobile-list

---
+++ components/UI/Link/mobile-list.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="absolute top-4 left-4 text-gray-500 dark:text-gray-300 text-xs flex items-center gap-2 font-semibold pl-0 p-2">
+    <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+    <li><Icon name="svg-spinners:pulse" class="w-5 h-5 md:w-6 md:h-6" /></li>
+    <li><NuxtLink to="/feed">Feed</NuxtLink></li>
+  </ul>
+</template>
