b738611d940726a4b606a955c090451999c27a92
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-11 18:46:42 +0700

    style: removed for tag <a>

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,12 +1,12 @@
 <template>
   <main class="bg-white dark:bg-gray-950 text-gray-600 dark:text-gray-400" id="blog">
-    <section
-      class="bg-[url('/images/pattern-hero-left.png')] dark:bg-[url('/images/pattern-hero-left-dark.png')] bg-contain bg-center px-8 lg:px-32"
-    >
+    <section class="bg-[url('/images/pattern-hero-left.png')] dark:bg-[url('/images/pattern-hero-left-dark.png')] bg-contain bg-center px-8 lg:px-32">
       <div class="container mx-auto bg-gray-100/50 dark:bg-gray-900/20 relative">
-        <UIButtonBack/>
+        <UIButtonBack />
         <div class="">
-          <UIButtonColorMode class="fixed top-4 right-4 rounded-md bg-gray-200 ring-1 ring-gray-400 dark:ring-0 lg:ring-0 dark:bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent dark:lg:bg-transparent lg:top-7 lg:right-9" />
+          <UIButtonColorMode
+            class="fixed top-4 right-4 rounded-md bg-gray-200 ring-1 ring-gray-400 dark:ring-0 lg:ring-0 dark:bg-white/10 px-2 py-1 lg:p-0 lg:bg-transparent dark:lg:bg-transparent lg:top-7 lg:right-9"
+          />
         </div>
         <div class="text-muted mx-auto py-12 px-8 lg:px-16 w-full lg:max-w-[40rem] bg-gray-500/10 dark:bg-gray-900/40">
           <ContentDoc />
@@ -15,9 +15,7 @@
               v-for="tag in data.tags"
               class="text-[10px] lg:text-xs w-[45%] lg:max-w-full font-semibold mt-4 lg:mt-0 py-2 px-2 lg:px-4 rounded-md text-gray-100 bg-blue-500 uppercase"
             >
-              <Icon name="pajamas:label" size="0.8rem" class="text-gray-100 mr-2" />{{
-                tag
-              }}
+              <Icon name="pajamas:label" size="0.8rem" class="text-gray-100 mr-2" />{{ tag }}
             </span>
           </div>
         </div>
@@ -39,7 +37,7 @@
 #blog p {
   @apply text-xs md:text-sm font-extralight md:font-normal !leading-loose py-2;
 }
-#blog a,
+
 #blog strong {
   @apply text-xs md:text-sm my-4;
 }
