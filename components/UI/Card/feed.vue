8564c3983fb542486def744e4a3de19a73dc3094
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-13 08:04:22 +0700

    docs: sort by publishDate

--- components/UI/Card/feed.vue
+++ components/UI/Card/feed.vue
@@ -1,9 +1,9 @@
 <script setup>
-const props = defineProps(["posts"]);
+const contentQuery = await queryContent("/feed").sort({ publishDate: -1 }).find();
 </script>
 
 <template>
-  <div v-for="post in props.posts" :key="post.slug" class="flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300">
+  <div v-for="{ _path: slug, publishDate, cover, description } in contentQuery" :key="slug" class="flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300">
     <div>
       <div class="flex justify-center items-center w-8 md:w-12 rotate-3 rounded-full">
         <img src="/images/hero-mobile.webp" alt="Photo" class="w-full h-full rounded-full shadow-md shadow-gray-500/40" />
@@ -12,10 +12,10 @@ const props = defineProps(["posts"]);
     <div class="w-full overflow-hidden space-y-4">
       <div class="flex justify-between items tracking-wide">
         <h2 class="text-xs flex items-center gap-1"><Icon name="line-md:iconify1" class="mr-0.5" />Dewa <span class="text-[7px]">🇮🇩</span></h2>
-        <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ post.publishDate }}</p>
+        <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ publishDate }}</p>
       </div>
-      <p class="text-xs mt-4 lg:max-w-[65%] leading-loose">{{ post.description }}</p>
-      <img :src="`/images/feed/${post.cover}`" alt="Photo" class="object-contain w-full" />
+      <p class="text-xs mt-4 lg:max-w-[65%] leading-loose">{{ description }}</p>
+      <img :src="`/images/feed/${cover}`" alt="Photo" class="object-contain w-full" />
     </div>
   </div>
 </template>
