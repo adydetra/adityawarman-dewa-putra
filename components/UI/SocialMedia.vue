c0b400c2c3bfd3bbaae77490021120364104a139
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-15 17:18:13 +0700

    docs: clear

--- components/UI/SocialMedia.vue
+++ components/UI/SocialMedia.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="flex text-gray-500 dark:text-gray-400 xl:gap-7 2xl:gap-8">
     <li v-for="data in datas" :key="data.id">
-      <NuxtLink rel="me" :to="data.to" :title="data.title" target="_blank" class="cursor-pointer ease-in-out duration-300 hover:text-yellow-400 dark:hover:text-yellow-200">
+      <NuxtLink :to="data.to" :title="data.title" target="_blank" class="cursor-pointer ease-in-out duration-300 hover:text-yellow-400 dark:hover:text-yellow-200">
         <Icon :name="data.name" class="w-6 h-6 2xl:w-7 2xl:h-7" />
       </NuxtLink>
     </li>
