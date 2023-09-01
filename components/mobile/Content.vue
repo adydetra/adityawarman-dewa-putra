a91db7ab108b9b8354edd2345710b992d7757dc1
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-01 07:44:34 +0700

    chore: change image

--- components/mobile/Content.vue
+++ components/mobile/Content.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="flex justify-center gap-6 overflow-hidden pt-16 pb-12">
     <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl shadow-2xl shadow-yellow-200/10 -rotate-3">
-      <NuxtImg src="/images/hero-1.webp" alt="" placeholder loading="lazy" class="absolute h-full w-full object-cover" />
+      <NuxtImg src="/images/mobile-1.png" alt="" placeholder loading="lazy" class="absolute h-full w-full object-cover" />
     </div>
     <div class="relative aspect-[10/10] w-32 md:w-60 flex-none overflow-hidden rounded-xl">
       <NuxtImg src="/images/hero-2.webp" alt="" placeholder loading="lazy" class="absolute h-full w-full object-scale-down" />
     </div>
     <div class="relative aspect-[10/10] w-44 md:w-80 flex-none overflow-hidden rounded-xl shadow-2xl shadow-yellow-200/10 rotate-3">
-      <NuxtImg src="/images/hero-3.webp" alt="" placeholder loading="lazy" class="absolute h-full w-full object-cover" />
+      <NuxtImg src="/images/mobile-3.png" alt="" placeholder loading="lazy" class="absolute h-full w-full object-cover" />
     </div>
   </section>
 
