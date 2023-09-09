14fa8904b14a6abb8b984d3204b88a14c8db2877
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-09 19:35:12 +0700

    chore(card): change shadow-md to sm

--- components/atoms/card/blog.vue
+++ components/atoms/card/blog.vue
@@ -3,7 +3,7 @@ const props = defineProps(["posts"]);
 </script>
 
 <template>
-  <div v-for="post in props.posts" :key="post.slug" class="rounded-xl shadow-md overflow-hidden hover:opacity-75">
+  <div v-for="post in props.posts" :key="post.slug" class="rounded-xl shadow-sm overflow-hidden hover:opacity-75">
     <NuxtLink :to="post._path">
       <NuxtImg :src="`/images/blog/${post.cover}`" alt="Blog Post Cover Image" placeholder loading="lazy" class="w-full h-32 md:h-40 2xl:h-52 object-cover" />
       <div class="p-4 lg:px-5 2xl:p-6 text-gray-500 dark:text-gray-100 bg-white/10 h-full">
