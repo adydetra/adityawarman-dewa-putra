00266ccf81d456a78c8f648f3bfee261e7d1d547
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-09 19:51:21 +0700

    chore(blog-card): minimalistic card

--- components/atoms/card/blog.vue
+++ components/atoms/card/blog.vue
@@ -3,10 +3,10 @@ const props = defineProps(["posts"]);
 </script>
 
 <template>
-  <div v-for="post in props.posts" :key="post.slug" class="rounded-xl shadow-sm overflow-hidden hover:opacity-75">
+  <div v-for="post in props.posts" :key="post.slug" class="rounded-md shadow-sm overflow-hidden hover:opacity-75">
     <NuxtLink :to="post._path">
-      <NuxtImg :src="`/images/blog/${post.cover}`" alt="Blog Post Cover Image" placeholder loading="lazy" class="w-full h-32 md:h-40 2xl:h-52 object-cover" />
-      <div class="p-4 lg:px-5 2xl:p-6 text-gray-500 dark:text-gray-100 bg-white/10 h-full">
+      <NuxtImg :src="`/images/blog/${post.cover}`" alt="Blog Post Cover Image" placeholder loading="lazy" class="object-cover" />
+      <div class="p-4 lg:px-5 2xl:p-6 text-gray-500/80 dark:text-gray-400 bg-white/10 h-full">
         <h2 class="text-sm lg:text-md font-bold">{{ post.title }}</h2>
         <div class="flex justify-between items-center py-2">
           <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ post.publishDate }}</p>
