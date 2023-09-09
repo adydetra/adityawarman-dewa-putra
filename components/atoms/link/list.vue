3f3cee34e0950a1546f8c1e6b7a2a2f942ac2e02
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-09 07:30:11 +0700

    refactor: change layout

--- components/atoms/link/list.vue
+++ components/atoms/link/list.vue
@@ -1,10 +1,12 @@
 <template>
-  <ul class="absolute top-4 left-4 text-gray-500 dark:text-gray-300 text-xs flex items-center gap-2 font-semibold pl-0 p-2">
-    <slot/>
-    <li><NuxtLink to="/blog" title="Blog">Blog</NuxtLink></li>
-    <li><Icon name="svg-spinners:pulse" class="w-5 h-5 md:w-6 md:h-6" /></li>
-    <li><NuxtLink to="/feed" title="Feed">Feed</NuxtLink></li>
-    <li><Icon name="line-md:chevron-small-right" class="w-5 h-5 md:w-6 md:h-6 text-gray-300 dark:text-gray-700" /></li>
+  <div class="flex items-center absolute top-4 left-4 text-gray-500 dark:text-gray-300 text-xs gap-2 font-semibold pl-0 p-2">
+    <ul class="flex items-center">
+      <slot />
+      <li><NuxtLink to="/blog" title="Blog">Blog</NuxtLink></li>
+      <li><Icon name="svg-spinners:pulse" class="w-5 h-5 md:w-6 md:h-6" /></li>
+      <li><NuxtLink to="/feed" title="Feed">Feed</NuxtLink></li>
+      <li><Icon name="line-md:chevron-small-right" class="w-5 h-5 md:w-6 md:h-6 text-gray-300 dark:text-gray-700" /></li>
+    </ul>
     <ul class="flex items-center -mt-0.5 space-x-4 text-gray-400 dark:text-gray-500">
       <li>
         <NuxtLink to="/uses" title="Uses"><Icon name="line-md:heart" class="w-4 h-4" /></NuxtLink>
@@ -16,5 +18,5 @@
         <NuxtLink to="/achievements" title="Achievements"><Icon name="line-md:star" class="w-4 h-4" /></NuxtLink>
       </li>
     </ul>
-  </ul>
+  </div>
 </template>
