d66dad93af8fb160adfc01b785d5a8ff2b04b238
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-12 08:32:11 +0700

    style: format

--- components/atoms/card/feed.vue
+++ components/atoms/card/feed.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-for="status in statuses" :key="status.id" class="flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300">
- 
     <div>
       <div class="flex justify-center items-center w-8 md:w-12 rotate-3 rounded-full">
         <NuxtImg src="/images/hero-mobile.webp" alt="Photo" placeholder loading="lazy" class="w-full h-full rounded-full shadow-md shadow-gray-500/40" />
@@ -8,11 +7,11 @@
     </div>
     <div class="w-full overflow-hidden space-y-4">
       <NuxtLink :to="status.uri" title="Mastodon">
-      <div class="flex justify-between items tracking-wide">
-        <h2 class="text-xs flex items-center gap-1"><Icon name="line-md:mastodon" class="mr-0.5" />Dewa <span class="text-[7px]">🇮🇩</span></h2>
-        <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ formatDate(status.created_at) }}</p>
-      </div>
-    </NuxtLink>
+        <div class="flex justify-between items tracking-wide">
+          <h2 class="text-xs flex items-center gap-1"><Icon name="line-md:mastodon" class="mr-0.5" />Dewa <span class="text-[7px]">🇮🇩</span></h2>
+          <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ formatDate(status.created_at) }}</p>
+        </div>
+      </NuxtLink>
       <p class="text-xs mt-4 leading-loose" v-html="status.content"></p>
       <NuxtImg :src="status.media_attachments[0]?.url" v-if="status.media_attachments.length > 0" alt="Photo" placeholder loading="lazy" class="object-contain" />
     </div>
@@ -39,8 +38,8 @@ onMounted(async () => {
 const formatDate = (dateString) => {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
   return `${day}-${month}-${year}`;
 };
 </script>
