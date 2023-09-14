d58ced3d1176fb92c1798bfcc5711ddbc4a861ac
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-14 17:16:50 +0700

    chore: remove middleware

--- middleware/resetMeta.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const route = useRoute();
-  useSeoMeta({
-    title: () => (route.meta.title as string) || "",
-    titleTemplate: (title) => (title ? `${title} - Dewa` : "Adityawarman Dewa Putra"),
-    description: "This is my personal site, build using Nuxt Js",
-  });
-});
