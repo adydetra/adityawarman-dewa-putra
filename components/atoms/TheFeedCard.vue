41fca15a3ba6731c643e8724c6883b4d5dd11370
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-19 15:03:09 +0700

    feat: skeleton loader effect

--- components/atoms/TheFeedCard.vue
+++ components/atoms/TheFeedCard.vue
@@ -1,26 +1,42 @@
 <template>
-  <div v-for="status in filteredStatuses" :key="status.id" class="flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300">
-    <div>
-      <div class="flex justify-center items-center w-8 md:w-12 overflow-hidden">
-        <NuxtImg :src="status.account.avatar" alt="Photo" placeholder loading="lazy" class="rounded-full" />
+  <div v-if="isLoading" class="space-y-5">
+    <div v-for="index in 4" :key="index" :class="cardStyle">
+      <div>
+        <div class="flex justify-center items-center w-8 md:w-12 h-8 md:h-12 bg-gray-400/40 rounded-full"></div>
+      </div>
+      <div class="w-full space-y-4">
+        <div class="bg-gray-400/40 h-3 w-1/2"></div>
+        <div class="bg-gray-400/40 h-3 w-3/4"></div>
+        <div class="bg-gray-400/40 h-3 w-2/3"></div>
+        <div class="bg-gray-400/40 h-3 w-4/5"></div>
       </div>
     </div>
-    <div class="w-full overflow-hidden space-y-4">
-      <NuxtLink :to="status.uri" title="Mastodon">
-        <div class="flex justify-between items tracking-wide">
-          <h2 class="text-xs flex items-center gap-1"><Icon name="line-md:mastodon" class="mr-0.5" />Dewa <span class="text-[7px]">🇮🇩</span></h2>
-          <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ formatDate(status.created_at) }}</p>
+  </div>
+
+  <div v-else class="space-y-5">
+    <div v-for="status in filteredStatuses" :key="status.id" :class="cardStyle">
+      <div>
+        <div class="flex justify-center items-center w-8 md:w-12 overflow-hidden">
+          <NuxtImg :src="status.account.avatar" alt="Photo" placeholder loading="lazy" class="rounded-full" />
         </div>
-      </NuxtLink>
-      <p class="text-xs mt-4 leading-loose" v-html="(status.content)"></p>
-      <NuxtImg
-        :src="status.media_attachments[0]?.url"
-        v-if="status.media_attachments && status.media_attachments.length > 0"
-        alt="Photo"
-        placeholder
-        loading="lazy"
-        class="object-contain"
-      />
+      </div>
+      <div class="w-full overflow-hidden space-y-4">
+        <NuxtLink :to="status.uri" title="Mastodon">
+          <div class="flex justify-between items tracking-wide">
+            <h2 class="text-xs flex items-center gap-1"><Icon name="line-md:mastodon" class="mr-0.5" />Dewa <span class="text-[7px]">🇮🇩</span></h2>
+            <p class="text-xs"><Icon name="line-md:calendar" class="mr-1" />{{ formatDate(status.created_at) }}</p>
+          </div>
+        </NuxtLink>
+        <p class="text-xs mt-4 leading-loose" v-html="status.content"></p>
+        <NuxtImg
+          :src="status.media_attachments[0]?.url"
+          v-if="status.media_attachments && status.media_attachments.length > 0"
+          alt="Photo"
+          placeholder
+          loading="lazy"
+          class="object-contain"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -28,6 +44,8 @@
 <script setup lang="ts">
 const statuses = ref<any[]>([]);
 const filteredStatuses = ref<any[]>([]);
+const isLoading = ref<boolean>(true);
+const cardStyle = "flex space-x-4 bg-gray-300/30 first-letter:dark:bg-slate-800 p-4 text-gray-600 dark:text-gray-300";
 
 const hasVideoAttachment = (status: any) => {
   return status.media_attachments && status.media_attachments.length > 0 && status.media_attachments[0].type === "video";
@@ -48,6 +66,7 @@ onMounted(async () => {
       const data = await response.json();
       statuses.value = data;
       filteredStatuses.value = data.filter((status: any) => !hasVideoAttachment(status));
+      isLoading.value = false;
     } else {
       console.error("Failed to fetch data from Mastodon API");
     }
--- pages/feed.vue
+++ pages/feed.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
     <h1 class="text-4xl lg:text-5xl text-center font-bold py-10 lg:py-12">Feed</h1>
-    <section class="flex lg:w-[55%] 2xl:w-[45%] flex-col mx-auto space-y-5 mt-4 lg:mt-8">
+    <section class="flex lg:w-[55%] 2xl:w-[45%] flex-col mx-auto mt-4 lg:mt-8">
       <AtomsTheFeedCard />
     </section>
   </NuxtLayout>
