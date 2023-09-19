bda2dca1e0c2271f3c0506f52bacaf0da56b49df
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-19 14:26:38 +0700

    chore: change Nuxt Js to Nuxt

--- app.vue
+++ app.vue
@@ -17,6 +17,6 @@ const route = useRoute();
 useSeoMeta({
   title: () => (route.meta.title as string) || "",
   titleTemplate: (title) => (title ? `${title} - Dewa` : "Adityawarman Dewa Putra"),
-  description: "This is my personal site, build using Nuxt Js",
+  description: "This is my personal site, build using Nuxt",
 });
 </script>
