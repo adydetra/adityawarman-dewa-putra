bc2035a788b7a2a9e0a8055e98ec7c546405ffe9
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-30 06:44:50 +0700

    chore(TheUsesCard): better styling

--- components/atoms/TheUsesCard.vue
+++ components/atoms/TheUsesCard.vue
@@ -3,7 +3,7 @@
   <div class="col-span-8 lg:col-span-2" :class="card">
     <div>
       <h2 :class="h2">OS</h2>
-      <ul :class="list" class="gap-6">
+      <ul :class="list">
         <li v-for="data in operatings" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -16,7 +16,7 @@
   <div class="col-span-8 lg:col-span-4" :class="card">
     <div>
       <h2 :class="h2">Workstation</h2>
-      <ul :class="list" class="gap-6">
+      <ul :class="list">
         <li v-for="data in workstations" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -29,7 +29,7 @@
   <div class="col-span-8 lg:col-span-2" :class="card">
     <div>
       <h2 :class="h2">Database</h2>
-      <ul :class="list" class="gap-6">
+      <ul :class="list">
         <li v-for="data in databases" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -42,7 +42,7 @@
   <div class="lg:backdrop-saturate-200 col-span-8 lg:col-span-4" :class="card">
     <div>
       <h2 :class="h2">Package Manager</h2>
-      <ul :class="list" class="gap-6">
+      <ul :class="list">
         <li v-for="data in packages" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -55,7 +55,7 @@
   <div class="lg:backdrop-saturate-200 col-span-8 lg:col-span-4" :class="card">
     <div>
       <h2 :class="h2">Tech Stack</h2>
-      <ul :class="list" class="gap-6 2xl:gap-8">
+      <ul :class="list">
         <li v-for="data in techstacks" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -68,7 +68,7 @@
   <div class="col-span-8" :class="card">
     <div>
       <h2 :class="h2">Development tools</h2>
-      <ul :class="list" class="gap-6 lg:gap-10">
+      <ul :class="list" class="lg:gap-10">
         <li v-for="data in developments" :key="data.id">
           <NuxtLink :to="data.to" target="_blank" :title="data.title" :class="link">
             <Icon :name="data.name" :class="icon" />
@@ -90,8 +90,8 @@ const techstacks = use.techstacks;
 const developments = use.developments;
 
 const card = "flex justify-center pt-6 pb-8 bg-gray-200/30 dark:bg-gray-500/5 rounded-md text-gray-400";
-const h2 = "text-sm md:text-md 2xl:text-lg font-light mb-3 2xl:mb-6 text-center";
-const list = "flex flex-wrap justify-center";
+const h2 = "text-sm md:text-md 2xl:text-lg font-light mb-6 lg:mb-8 text-center";
+const list = "flex flex-wrap justify-center px-16 md:!px-0 gap-8 md:gap-12";
 const link = "cursor-pointer ease-in-out duration-300 dark:text-gray-600 hover:text-yellow-400 dark:hover:text-yellow-200";
-const icon = "w-3 h-3 lg:w-4 lg:h-4 2xl:w-5 2xl:h-5";
+const icon = "w-3.5 h-3.5 md:w-4 md:h-4 2xl:w-5 2xl:h-5";
 </script>
