6b2c28f0e54b3f51384d9c1425a38130a151a3d4
devdewa <uchiasasuke123.sp@gmail.com>
2023-10-25 04:34:26 +0700

    chore: insert to a component

--- layouts/mobile.vue
+++ layouts/mobile.vue
@@ -3,43 +3,7 @@
     <AtomsColorMode class="fixed z-10 flex items-center top-4 right-4 rounded-md bg-gray-200 ring-1 ring-gray-400 dark:ring-0 dark:bg-white/10 p-2 dark:lg:bg-transparent" />
     <div class="pt-20 pb-8 px-4">
       <AtomsNav />
-      <p class="text-black dark:text-gray-300 text-2xl font-bold">A.<span class="text-4xl">Dewa</span> Putra</p>
-      <div class="mt-6 text-gray-400 text-sm leading-loose">
-        <div class="space-y-4">
-          <p>~ Hello, I am Adityawarman <span class="px-1 rounded-md bg-gray-200/50 dark:bg-gray-900">Dewa</span> Putra. Front End Developer, big fan open source, enjoying life.</p>
-          <p>Study at <NuxtLink to="https://www.ut.ac.id/" title="Terbuka University" :class="link">Terbuka University.</NuxtLink></p>
-          <p>
-            Dreaming up ideas and making them come true is where my passion lies. You can find my
-            <NuxtLink to="/work" title="Work" :class="link">full work list here</NuxtLink>
-          </p>
-          <p>
-            Outside of programming, I enjoy watching streamers and playing games. If you happen to be in the same city as where I live (currently in Jakarta), maybe we could
-            hang out and talk about technology or code together.
-          </p>
-        </div>
-        <div class="mt-12">
-          <p>Find Me On</p>
-          <ul class="mt-2 flex text-gray-500 dark:text-gray-400 gap-8 md:gap-7">
-            <li v-for="data in social" :key="data.id">
-              <NuxtLink :to="data.to" :title="data.title" target="_blank" class="cursor-pointer ease-in-out duration-300 hover:text-yellow-200">
-                <Icon :name="data.name" class="w-5 h-5 md:w-6 md:h-6" />
-              </NuxtLink>
-            </li>
-          </ul>
-          <p class="mt-8">
-            If you enjoy my works, consider sponsoring me on
-            <NuxtLink to="https://github.com/sponsors/adydetra" title="GitHub Sponsor" :class="link">GitHub Sponsor.</NuxtLink>
-          </p>
-        </div>
-        <AtomsFooter class="lg:hidden" />
-      </div>
+      <AtomsProfile />
     </div>
   </section>
 </template>
-
-<script setup lang="ts">
-import * as data from "~/types/index";
-
-const social = data.social;
-const link = "text-gray-600 dark:text-gray-300 pb-0.5 border-b-2 border-gray-200/50 dark:border-gray-700/40";
-</script>
