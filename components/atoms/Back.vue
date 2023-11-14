ae31c109019b941d46e1c784178fc92abfcdd69b
devdewa <uchiasasuke123.sp@gmail.com>
2023-11-14 13:48:57 +0700

    perf(Back): covert option api to composition api

--- components/atoms/Back.vue
+++ components/atoms/Back.vue
@@ -1,18 +1,12 @@
 <template>
-  <button
-    @click="goToPrev()"
-    class="fixed top-4 left-4"
-  >
+  <button @click="goToPrev()" class="fixed top-4 left-4">
     <Icon name="line-md:arrow-left-circle" class="text-black dark:text-white w-4 h-4 lg:w-6 lg:h-6" />
   </button>
 </template>
 
-<script>
-export default {
-  methods: {
-    goToPrev() {
-      window.history.length > 1 ? this.$router.back() : this.$router.push("/");
-    },
-  },
+<script lang="ts" setup>
+const route = useRouter();
+const goToPrev = () => {
+  window.history.length > 1 ? route.back() : route.push("/");
 };
 </script>
