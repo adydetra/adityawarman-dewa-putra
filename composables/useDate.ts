38b27513f922c5d954ea59608f60b23be1968354
devdewa <uchiasasuke123.sp@gmail.com>
2023-12-27 02:58:56 +0700

    refactor: mix date composable

--- composables/useDate.ts
+++ composables/useDate.ts
@@ -1,4 +1,15 @@
-export default function () {
+export function useDateFeed() {
+  const formatDateFeed = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+  return formatDateFeed;
+}
+
+export function useDateDemo() {
   const formatDateDemo = (dateString: string) => {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
