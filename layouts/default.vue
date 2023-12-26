9d37bd03c189e50b875d5659f4b8b0cceb09117c
devdewa <uchiasasuke123.sp@gmail.com>
2023-12-27 03:32:44 +0700

    refactor: clean code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 const layout = 'hidden lg:flex flex-wrap justify-center items-center xl:w-40 2xl:w-52 py-6 mx-auto h-screen bg-white dark:bg-gray-950 fixed top-0';
 const link = 'text-xs text-gray-400 hover:text-yellow-500 dark:hover:text-yellow-300 text-center w-full';
+const box = 'w-full flex justify-center my-8';
 
 const route = useRoute();
 const routeMap: Record<string, number> = {
@@ -19,12 +20,12 @@ const showRightContent = currentNumber !== 4;
 const rightLink = showRightContent ? (currentNumber === 1 ? 'uses' : currentNumber === 2 ? 'work' : currentNumber === 3 ? 'closing' : '#') : '#';
 const rightIconName = showRightContent ? (currentNumber === 1 ? 'ci:chevron-right-duo' : 'ci:chevron-right-duo') : '#';
 
-const leftContentClass = showLeftContent
-  ? 'w-full flex justify-center cursor-pointer py-8 ease-in-out duration-300 text-slate-400 hover:text-yellow-400 dark:hover:text-yellow-200'
-  : 'w-full flex justify-center cursor-not-allowed py-8 ease-in-out duration-300 text-slate-400 hover:text-red-400';
-const rightContentClass = showRightContent
-  ? 'w-full flex justify-center cursor-pointer py-8 ease-in-out duration-300 text-slate-400 hover:text-yellow-400 dark:hover:text-yellow-200'
-  : 'w-full flex justify-center cursor-not-allowed py-8 ease-in-out duration-300 text-slate-400 hover:text-red-400';
+const activeLeft = showLeftContent
+  ? 'cursor-pointer hover:text-yellow-400 dark:hover:text-yellow-200'
+  : 'cursor-not-allowed hover:text-red-400';
+const activeRight = showRightContent
+  ? 'cursor-pointer hover:text-yellow-400 dark:hover:text-yellow-200'
+  : 'cursor-not-allowed hover:text-red-400';
 </script>
 
 <template>
@@ -33,15 +34,17 @@ const rightContentClass = showRightContent
       <AtomsLine>
         <AtomsNumber :number="currentNumber" />
       </AtomsLine>
-      <NuxtLink
-        :to="leftLink"
-        :title="currentNumber === 1 ? '' : currentNumber === 2 ? 'Home' : currentNumber === 3 ? 'Uses' : 'Work'"
-        :class="leftContentClass"
-        aria-label="slide"
-      >
-        <Icon v-if="showLeftContent" :name="leftIconName" class="animate-pulse w-4 h-4 lg:w-6 lg:h-6" />
-        <Icon v-else name="mingcute:dot-grid-line" class="w-4 h-4 lg:w-6 lg:h-6" />
-      </NuxtLink>
+      <div :class="box">
+        <NuxtLink
+          :to="leftLink"
+          :title="currentNumber === 1 ? '' : currentNumber === 2 ? 'Home' : currentNumber === 3 ? 'Uses' : 'Work'"
+          :class="`ease-in-out duration-300 text-slate-400 ${activeLeft}`"
+          aria-label="slide"
+        >
+          <Icon v-if="showLeftContent" :name="leftIconName" class="animate-pulse w-4 h-4 lg:w-6 lg:h-6" />
+          <Icon v-else name="mingcute:dot-grid-line" class="w-4 h-4 lg:w-6 lg:h-6" />
+        </NuxtLink>
+      </div>
       <AtomsLine>
         <NuxtLink to="/demo" title="Redirect to Demo" class="-rotate-45" :class="link">
           Demo
@@ -51,17 +54,21 @@ const rightContentClass = showRightContent
     <slot />
     <div class="fixed top-0 right-0" :class="layout">
       <AtomsLine>
-        <AtomsColorMode class="w-full" />
+        <div class="w-full flex justify-center">
+          <AtomsColorMode />
+        </div>
       </AtomsLine>
-      <NuxtLink
-        :to="rightLink"
-        :title="currentNumber === 1 ? 'Uses' : currentNumber === 2 ? 'Work' : currentNumber === 3 ? 'Closing' : ''"
-        :class="rightContentClass"
-        aria-label="slide"
-      >
-        <Icon v-if="showRightContent" :name="rightIconName" class="animate-pulse w-4 h-4 lg:w-6 lg:h-6" />
-        <Icon v-else name="mingcute:dot-grid-line" class="w-4 h-4 lg:w-6 lg:h-6" />
-      </NuxtLink>
+      <div :class="box">
+        <NuxtLink
+          :to="rightLink"
+          :title="currentNumber === 1 ? 'Uses' : currentNumber === 2 ? 'Work' : currentNumber === 3 ? 'Closing' : ''"
+          :class="`ease-in-out duration-300 text-slate-400 ${activeRight}`"
+          aria-label="slide"
+        >
+          <Icon v-if="showRightContent" :name="rightIconName" class="animate-pulse w-4 h-4 lg:w-6 lg:h-6" />
+          <Icon v-else name="mingcute:dot-grid-line" class="w-4 h-4 lg:w-6 lg:h-6" />
+        </NuxtLink>
+      </div>
       <AtomsLine>
         <NuxtLink to="/feed" title="Redirect to Feed" class="rotate-45" :class="link">
           Feed
