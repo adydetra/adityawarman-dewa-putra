0d755de771371a2c8e7486b6470c07a9c303d046
devdewa <uchiasasuke123.sp@gmail.com>
2024-01-06 23:43:59 +0700

    chore: remove unused code

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,8 @@
 import antfu from '@antfu/eslint-config';
-import format from 'eslint-plugin-format';
 
 export default antfu({
+  formatters: true,
   stylistic: {
     semi: true,
-  },
-  formatters: {
-    css: true,
-    html: true,
-    markdown: 'prettier',
-  },
-  files: ['**/*.css'],
-  languageOptions: {
-    parser: format.parserPlain,
-  },
-  plugins: {
-    format,
-  },
-  rules: {
-    'format/prettier': ['error', { parser: 'css', tabWidth: 2 }],
-  },
-  files: ['**/*.toml'],
-  languageOptions: {
-    parser: format.parserPlain,
-  },
-  plugins: {
-    format,
-  },
-  rules: {
-    'format/dprint': ['error', { language: 'toml', languageOptions: { indentWidth: 2 } }],
   },
 });
