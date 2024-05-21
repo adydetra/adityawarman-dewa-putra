cb0c26e12401e583b05a7cf878f5c4e299559304
devdewa <uchiasasuke123.sp@gmail.com>
2024-05-22 04:37:36 +0700

    refactor: minimize code

--- src/components/molecules/TheUsesCard.vue
+++ src/components/molecules/TheUsesCard.vue
@@ -1,104 +1,32 @@
 <script setup lang="ts">
-const operatings = useOperatings();
-const workstations = useWorkstations();
-const databases = useDatabases();
-const packages = usePackages();
-const techstacks = useTechstacks();
-const developments = useDevelopments();
+const sections = ref([
+  { title: 'OS', data: useOperatings(), colSpan: 'lg:col-span-2' },
+  { title: 'Workstation', data: useWorkstations(), colSpan: 'lg:col-span-4' },
+  { title: 'Database', data: useDatabases(), colSpan: 'lg:col-span-2' },
+  { title: 'Package Manager', data: usePackages(), colSpan: 'lg:col-span-4' },
+  { title: 'Tech Stack', data: useTechstacks(), colSpan: 'lg:col-span-4' },
+  { title: 'Development tools', data: useDevelopments(), colSpan: 'col-span-8' },
+]);
 
-const card = 'flex justify-center pt-6 pb-8 bg-gray-200/30 dark:bg-gray-500/5 rounded-md text-gray-400 transition-colors ease-in-out duration-500';
-const h2 = 'text-sm md:text-md 2xl:text-lg font-light mb-6 lg:mb-8 text-center';
-const list = 'flex flex-wrap justify-center px-16 md:!px-0 gap-8 md:gap-12';
-const link = 'cursor-pointer transition-colors ease-in-out duration-300 dark:text-gray-600 hover:text-yellow-400 dark:hover:text-yellow-200';
-const icon = 'size-3.5 md:size-4 2xl:size-5';
+const classes = {
+  card: 'flex justify-center pt-6 pb-8 bg-gray-200/30 dark:bg-gray-500/5 rounded-md text-gray-400 transition-colors ease-in-out duration-500',
+  h2: 'text-sm md:text-md 2xl:text-lg font-light mb-6 lg:mb-8 text-center',
+  list: 'flex flex-wrap justify-center px-16 md:!px-0 gap-8 md:gap-12',
+  link: 'cursor-pointer transition-colors ease-in-out duration-300 dark:text-gray-600 hover:text-yellow-400 dark:hover:text-yellow-200',
+  icon: 'size-3.5 md:size-4 2xl:size-5',
+};
 </script>
 
 <template>
-  <!-- Operating System -->
-  <div class="col-span-8 lg:col-span-2" :class="card">
+  <div v-for="(section, index) in sections" :key="index" class="col-span-8" :class="[classes.card, section.colSpan]">
     <div>
-      <h2 :class="h2">
-        OS
+      <h2 :class="classes.h2">
+        {{ section.title }}
       </h2>
-      <ul :class="list">
-        <li v-for="(data, index) in operatings" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Workstation -->
-  <div class="col-span-8 lg:col-span-4" :class="card">
-    <div>
-      <h2 :class="h2">
-        Workstation
-      </h2>
-      <ul :class="list">
-        <li v-for="(data, index) in workstations" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Database -->
-  <div class="col-span-8 lg:col-span-2" :class="card">
-    <div>
-      <h2 :class="h2">
-        Database
-      </h2>
-      <ul :class="list">
-        <li v-for="(data, index) in databases" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Package Manager -->
-  <div class="lg:backdrop-saturate-200 col-span-8 lg:col-span-4" :class="card">
-    <div>
-      <h2 :class="h2">
-        Package Manager
-      </h2>
-      <ul :class="list">
-        <li v-for="(data, index) in packages" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Tech Stack Development -->
-  <div class="lg:backdrop-saturate-200 col-span-8 lg:col-span-4" :class="card">
-    <div>
-      <h2 :class="h2">
-        Tech Stack
-      </h2>
-      <ul :class="list">
-        <li v-for="(data, index) in techstacks" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Development tools -->
-  <div class="col-span-8" :class="card">
-    <div>
-      <h2 :class="h2">
-        Development tools
-      </h2>
-      <ul :class="list" class="lg:gap-10">
-        <li v-for="(data, index) in developments" :key="index">
-          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="link">
-            <Icon :name="data.icon" :class="icon" />
+      <ul :class="classes.list">
+        <li v-for="(data, idx) in section.data" :key="idx">
+          <NuxtLink :to="`https://${data.url}`" target="_blank" :title="data.title" :class="classes.link">
+            <Icon :name="data.icon" :class="classes.icon" />
           </NuxtLink>
         </li>
       </ul>
--- src/composables/useUses.ts
+++ src/composables/useUses.ts
@@ -5,43 +5,39 @@ export interface Uses {
 }
 
 export function useOperatings(): Uses[] {
-  const operatings: Uses[] = [
+  return [
     { icon: 'uim:windows', title: 'Windows', url: 'www.microsoft.com/en-us/windows' },
     { icon: 'cib:arch-linux', title: 'Arch Linux', url: 'archlinux.org' },
   ];
-  return operatings;
 }
 
 export function useWorkstations(): Uses[] {
-  const workstations: Uses[] = [
+  return [
     { icon: 'ic:round-smartphone', title: 'Xiaomi Redmi Note 7 Pro', url: 'www.gsmarena.com/xiaomi_redmi_note_7-9513.php' },
     { icon: 'solar:laptop-outline', title: 'Asus A412DA Ryzen 3', url: 'www.reviewlaptop-id.com/asus-a412da-ek301t-amd-ryzen-3-3200u' },
     { icon: 'tabler:rectangle', title: 'Paradox Tarot', url: 'paradoxgaming.id/produk/fox-mask-tarot' },
     { icon: 'streamline:entertainment-earpods-airpods-audio-earpods-music-earbuds-true-wireless', title: 'HUAWEI FreeBuds SE', url: 'consumer.huawei.com/en/headphones/freebuds-se' },
   ];
-  return workstations;
 }
 
 export function useDatabases(): Uses[] {
-  const databases: Uses[] = [
+  return [
     { icon: 'tabler:brand-mysql', title: 'MySQL', url: 'www.mysql.com' },
     { icon: 'akar-icons:postgresql-fill', title: 'PostgreSQL', url: 'www.postgresql.org' },
   ];
-  return databases;
 }
 
 export function usePackages(): Uses[] {
-  const packages: Uses[] = [
+  return [
     { icon: 'simple-icons:npm', title: 'NPM', url: 'www.npmjs.com' },
     { icon: 'akar-icons:yarn-fill', title: 'Yarn', url: 'yarnpkg.com' },
     { icon: 'file-icons:pnpm', title: 'PNPM', url: 'pnpm.io' },
     { icon: 'simple-icons:bun', title: 'Bun', url: 'bun.sh' },
   ];
-  return packages;
 }
 
 export function useTechstacks(): Uses[] {
-  const techstacks: Uses[] = [
+  return [
     { icon: 'simple-icons:tailwindcss', title: 'TailwindCSS (UI)', url: 'tailwindcss.com' },
     { icon: 'simple-icons:unocss', title: 'UnoCSS (UI)', url: 'unocss.dev' },
     { icon: 'simple-icons:vuedotjs', title: 'Vue (JS Framework)', url: 'vuejs.org' },
@@ -50,11 +46,10 @@ export function useTechstacks(): Uses[] {
     { icon: 'devicon-plain:vitest', title: 'Vitest (Testing)', url: 'vitest.dev' },
     { icon: 'devicon-plain:typescript', title: 'TypeScript (Language)', url: 'www.typescriptlang.org' },
   ];
-  return techstacks;
 }
 
 export function useDevelopments(): Uses[] {
-  const developments: Uses[] = [
+  return [
     { icon: 'akar-icons:vscode-fill', title: 'VS Code', url: 'code.visualstudio.com' },
     { icon: 'cib:figma', title: 'FIGMA', url: 'www.figma.com' },
     { icon: 'bi:git', title: 'Git', url: 'git-scm.com' },
@@ -63,5 +58,4 @@ export function useDevelopments(): Uses[] {
     { icon: 'simple-icons:firefoxbrowser', title: 'Firefox Developer Edition', url: 'www.mozilla.org/id/firefox/developer' },
     { icon: 'NuxtIcon', title: 'Arc', url: 'arc.net' },
   ];
-  return developments;
 }
