381c6f0f6aaa38a4bdd89b0105f971c4ae6833d3
adydetra <devdewa123.sp@gmail.com>
2025-05-19 04:04:02 +0700

    chore: add new capture img

--- src/components/molecules/ThePhotos.vue
+++ src/components/molecules/ThePhotos.vue
@@ -5,7 +5,7 @@ interface Photo {
   alt: string;
 }
 
-const totalPhotos = 115;
+const totalPhotos = 125;
 const photos = Array.from({ length: totalPhotos }, (_, i) => ({
   id: i + 1,
   src: `/images/capture/photo${i + 1}.jpg`,
