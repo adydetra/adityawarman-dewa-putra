fda0f4cfb61765025c5213d9b4d33d8408a55ca5
devdewa <uchiasasuke123.sp@gmail.com>
2025-09-01 22:56:11 +0700

    chore: logic change in load photos

--- app/components/molecules/ThePhotos.vue
+++ app/components/molecules/ThePhotos.vue
@@ -13,76 +13,56 @@ const photos = Array.from({ length: totalPhotos }, (_, i) => ({
 })).reverse();
 
 const selectedPhoto = ref<Photo | null>(null);
+const popupImageLoaded = ref(false);
 
 function openPopup(photo: Photo) {
   selectedPhoto.value = photo;
+
+  const img = new Image();
+  img.onload = () => {
+    popupImageLoaded.value = true;
+  };
+  img.onerror = () => {
+    popupImageLoaded.value = false;
+  };
+  img.src = photo.src;
+
+  if (img.complete) {
+    popupImageLoaded.value = true;
+  }
+  else {
+    popupImageLoaded.value = false;
+  }
 }
 
 function closePopup() {
   selectedPhoto.value = null;
+  popupImageLoaded.value = false;
 }
 </script>
 
 <template>
   <section>
     <div class="grid grid-cols-2 md:grid-cols-3 3xl:grid-cols-4 gap-4 mb-8 lg:px-20 xl:px-60 3xl:px-8">
-      <div
-        v-for="photo in photos"
-        :key="photo.id"
-        class="aspect-square overflow-hidden cursor-pointer mx-auto"
-        @click="openPopup(photo)"
-      >
-        <NuxtImg
-          v-slot="{ imgAttrs, isLoaded }"
-          custom
-          :src="photo.src"
-          :alt="photo.alt"
-          loading="lazy"
-        >
+      <div v-for="photo in photos" :key="photo.id" class="aspect-square overflow-hidden cursor-pointer mx-auto" @click="openPopup(photo)">
+        <NuxtImg v-slot="{ imgAttrs, isLoaded }" custom :src="photo.src" :alt="photo.alt" loading="lazy">
           <div class="relative size-full">
-            <img
-              v-bind="imgAttrs"
-              class="object-cover size-full transition-opacity duration-300"
-            >
-            <div
-              v-if="!isLoaded"
-              class="absolute inset-0 flex items-center justify-center bg-gray-100 dark:bg-gray-800"
-            >
-              <Icon
-                name="line-md:loading-twotone-loop"
-                class="size-4 lg:size-6 animate-spin dark:text-gray-300"
-              />
+            <img v-bind="imgAttrs" class="object-cover size-full transition-opacity duration-300">
+            <div v-if="!isLoaded" class="absolute inset-0 flex items-center justify-center bg-gray-100 dark:bg-gray-800">
+              <Icon name="line-md:loading-twotone-loop" class="size-4 lg:size-6 animate-spin dark:text-gray-300" />
             </div>
           </div>
         </NuxtImg>
       </div>
     </div>
 
-    <div
-      v-if="selectedPhoto"
-      class="fixed inset-0 flex items-center justify-center z-50 bg-black/50 backdrop-blur-md"
-      @click="closePopup"
-    >
+    <div v-if="selectedPhoto" class="fixed inset-0 flex items-center justify-center z-50 bg-black/50 backdrop-blur-md" @click="closePopup">
       <div class="xl:h-[80vh]" @click.stop>
-        <NuxtImg
-          v-slot="{ imgAttrs, isLoaded }"
-          custom
-          :src="selectedPhoto.src"
-          :alt="selectedPhoto.alt"
-        >
+        <NuxtImg v-slot="{ imgAttrs }" custom :src="selectedPhoto.src" :alt="selectedPhoto.alt">
           <div class="relative size-full">
-            <img
-              v-bind="imgAttrs"
-              class="object-contain size-full transition-opacity duration-300"
-            >
-            <div
-              v-if="!isLoaded"
-              class="absolute inset-0 flex items-center justify-center bg-black"
-            >
-              <Icon
-                name="line-md:loading-twotone-loop"
-                class="size-6 animate-spin dark:text-gray-300"
-              />
+            <img v-bind="imgAttrs" class="object-contain size-full transition-opacity duration-300" @load="popupImageLoaded = true">
+            <div v-if="!popupImageLoaded" class="absolute inset-0 flex items-center justify-center bg-black">
+              <Icon name="line-md:loading-twotone-loop" class="size-6 animate-spin dark:text-gray-300" />
             </div>
           </div>
         </NuxtImg>
