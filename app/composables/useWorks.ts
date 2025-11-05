96c1ad500b35c06ed2e5bb7be10bfb84505755f7
adydetra <devdewa123.sp@gmail.com>
2025-11-05 20:55:48 +0700

    feat: add Video Music Converter project to works list

--- app/composables/useWorks.ts
+++ app/composables/useWorks.ts
@@ -39,6 +39,7 @@ export function useWorks(): Works[] {
     { icon: 'line-md:external-link', title: 'GLB GLTF Viewer', tech: 'Three.js', ui: 'CSS', url: 'glb-gltf-viewer-adydetra.vercel.app' },
     { icon: 'line-md:external-link', title: 'Fingertune', tech: 'Three.js', ui: 'CSS', url: 'fingertune.vercel.app' },
     { icon: 'line-md:external-link', title: 'Black Hole ASCII', tech: 'Svelte', ui: 'CSS', url: 'black-hole-ascii.vercel.app' },
+    { icon: 'line-md:github-loop', title: 'Video Music Converter', tech: 'Python', ui: 'Tkinter', url: 'github.com/adydetra/video-music-converter' },
   ];
   return works;
 }
